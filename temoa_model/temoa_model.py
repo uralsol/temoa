59619cc6921cab6434451e773fa4906e35dd64de
Hadi Eshraghi <heshrag@ncsu.edu>
2020-05-07 16:22:16 -0500

    Define MGA-specific variable in temoa_run.py The derived decision variable V_ActivityByTech is now defined under the SolveWithMGA function in the temoa_run.py

--- temoa_model/temoa_model.py
+++ temoa_model/temoa_model.py
@@ -257,8 +257,6 @@ def temoa_create_model(name="Temoa"):
         M.CapacityAvailableVar_pt, domain=NonNegativeReals
     )
 
-    # This derived variable is used in MGA objective function:
-
     # ---------------------------------------------------------------
     # Declare the Objective Function.
     # ---------------------------------------------------------------
