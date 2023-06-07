89eacb6933c228513f80d8736a9926f4c0d88d97
Cameron Wade <cameron@sutubra.ca>
2023-06-07 13:23:40 -0300

    Patch bug in DemandActivityConstraint
    A value() function needed to be added to access the
    pyomo parameter value.

--- temoa_model/temoa_initialize.py
+++ temoa_model/temoa_initialize.py
@@ -1076,7 +1076,7 @@ ensure demand activity remains consistent across time slices.
 			for d0 in M.time_of_day:
 				if (r,p,s0,d0,dem) in M.DemandSpecificDistribution.sparse_keys():
 					try:
-						if M.DemandSpecificDistribution[r,p,s0,d0,dem] > eps:
+						if value(M.DemandSpecificDistribution[r,p,s0,d0,dem]) > eps:
 							break
 					except:
 						continue
